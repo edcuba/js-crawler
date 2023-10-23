de4e2b1d444a9d1f79fe014e694bad08ea6e883b
Eduard Cuba <cuba.edo@gmail.com>
2023-10-23 12:54:17 +0200

    Disable persistance to avoid deps between users

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,56 @@
-import { PlaywrightCrawler } from 'crawlee';
+import { PlaywrightCrawler, Configuration } from 'crawlee';
 import express from 'express';
 import fs from "fs";
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express()
 app.use(express.json());
 
 const port = 3000
 
+const config = new Configuration({
+    defaultBrowserPath: "/home/myuser/pw-browsers/chrome",
+    persistStorage: false,
+});
 
-app.post('/', async (req, res) => {
+
+app.post('/', async (req, res, next) => {
     const urls: Array<string> = [];
     const keys: Array<string> = [];
 
     const crawler = new PlaywrightCrawler({
         maxRequestsPerCrawl: 10,
-        async requestHandler({ page, request, enqueueLinks }) {
-            await enqueueLinks({
-                strategy: 'same-hostname',
-            });
-            const r = (Math.random() + 1).toString(36).substring(2);
-            const fileName = r + ".html";
+        async requestHandler({ page, request /*, enqueueLinks*/ }) {
+            // await enqueueLinks({
+            //      strategy: 'same-hostname',
+            // });
+            const uniqueKey = request.uniqueKey;
+            const fileName = uniqueKey + ".html";
+
             keys.push(fileName);
             urls.push(request.url);
             fs.writeFile("storage/" + fileName, await page.content(), function (err) {
                 if (err) return console.log(err);
             });
         },
-    });
+    }, config);
 
-    const url = req.body.url;
-    await crawler.run([url]);
+    console.log(req.body)
+    const requested_urls = req.body.urls;
+
+    try {
+        // assign unique key to each page
+        await crawler.run(requested_urls.map((url: string) => ({ url, uniqueKey: uuidv4() })));
+    } catch (e) {
+        return next(e);
+    }
 
     const response = {
         "keys": keys,
         "urls": urls,
     };
 
+    // response
     res.send(response);
 })
 
